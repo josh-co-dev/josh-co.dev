881304e27e6c5e066fa69fef17869f5d8f2dab3d
nmd102 <nmduncan@madison.k12.wi.us>
2024-05-22 09:53:49 -0500

    Actual code on c# and python tabs on josh-engine page

--- js/josh-engine.js
+++ js/josh-engine.js
@@ -1,61 +1,53 @@
 let selected = 2;
 let tl;
 const codes=  [
-    "<p class=\"comment\">// main.cpp</p>\n" +
-    "                        <span class=\"metadata\">#include</span> <span class=\"string\">\"engine/engine.h\"</span><br>\n" +
-    "                        <span class=\"metadata\">#include</span> <span class=\"string\">\"test.h\"</span><br><br>\n" +
-    "                        <span class=\"type\">int</span> <span class=\"function\">main</span><span>() {</span><br>\n" +
-    "                        &emsp;&emsp;<span class=\"type\">JEGraphicsSettings</span> <span class='var'>graphicsSettings</span><span>{};</span><br>\n" +
-    "                        &emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span\n" +
-    "                            class=\"property\">vsyncEnabled</span><span> = </span><span\n" +
-    "                            class=\"boolean\">true</span><span>;</span><br>\n" +
-    "                        &emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span\n" +
-    "                            class=\"property\">skybox</span><span> = </span><span\n" +
-    "                            class=\"boolean\">true</span><span>;</span><br>\n" +
-    "                        &emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span class=\"property\">clearColor</span><span>[</span><span\n" +
-    "                            class=\"number\">0</span><span>] = </span><span class=\"number\">0.75f</span><span>;</span><br>\n" +
-    "                        &emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span class=\"property\">clearColor</span><span>[</span><span\n" +
-    "                            class=\"number\">1</span><span>] = </span><span class=\"number\">0.75f</span><span>;</span><br>\n" +
-    "                        &emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span class=\"property\">clearColor</span><span>[</span><span\n" +
-    "                            class=\"number\">2</span><span>] = </span><span class=\"number\">0.8f</span><span>;</span><br>\n" +
-    "                        &emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span\n" +
-    "                            class=\"property\">msaaSamples</span><span> = </span><span\n" +
-    "                            class=\"number\">4</span><span>;</span><br>\n" +
-    "                        &emsp;&emsp;<span class='function'>init</span><span>(</span><span class=\"string\">\"JoshEngine Demo\"</span><span>, </span><span\n" +
-    "                            class=\"number\">1280</span><span>, </span><span class=\"number\">720</span><span>, </span><span class='var'>graphicsSettings</span><span>);</span><br>\n" +
-    "                        &emsp;&emsp;<span class='function'>setupTest</span><span>();</span><br>\n" +
-    "                        &emsp;&emsp;<span class='function'>mainLoop</span><span>();</span><br>\n" +
-    "                        &emsp;&emsp;<span class='function'>deinit</span><span>();</span><br>\n" +
-    "                        &emsp;&emsp;<span class=\"keyword\">return </span><span class=\"number\">0</span><span>;</span><br>\n" +
-    "                        <span>}</span>\n",
+    "<p class=\"comment\">// main.cpp</p>" +
+    "<span class=\"metadata\">#include</span> <span class=\"string\">\"engine/engine.h\"</span><br>" +
+    "<span class=\"metadata\">#include</span> <span class=\"string\">\"test.h\"</span><br><br>" +
+    "<span class=\"type\">int</span> <span class=\"function\">main</span><span>() {</span><br>" +
+    "&emsp;&emsp;<span class=\"type\">JEGraphicsSettings</span> <span class='var'>graphicsSettings</span><span>{};</span><br>" +
+    "&emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span" +
+    " class=\"property\">vsyncEnabled</span><span> = </span><span" +
+    " class=\"boolean\">true</span><span>;</span><br>" +
+    "&emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span" +
+    " class=\"property\">skybox</span><span> = </span><span" +
+    " class=\"boolean\">true</span><span>;</span><br>" +
+    "&emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span class=\"property\">clearColor</span><span>[</span><span" +
+    " class=\"number\">0</span><span>] = </span><span class=\"number\">0.75f</span><span>;</span><br>" +
+    "&emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span class=\"property\">clearColor</span><span>[</span><span" +
+    " class=\"number\">1</span><span>] = </span><span class=\"number\">0.75f</span><span>;</span><br>" +
+    "&emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span class=\"property\">clearColor</span><span>[</span><span" +
+    " class=\"number\">2</span><span>] = </span><span class=\"number\">0.8f</span><span>;</span><br>" +
+    "&emsp;&emsp;<span class='var'>graphicsSettings</span><span>.</span><span" +
+    " class=\"property\">msaaSamples</span><span> = </span><span" +
+    " class=\"number\">4</span><span>;</span><br>" +
+    "&emsp;&emsp;<span class='function'>init</span><span>(</span><span class=\"string\">\"JoshEngine Demo\"</span><span>, </span><span" +
+    " class=\"number\">1280</span><span>, </span><span class=\"number\">720</span><span>, </span><span class='var'>graphicsSettings</span><span>);</span><br>" +
+    "&emsp;&emsp;<span class='function'>setupTest</span><span>();</span><br>" +
+    "&emsp;&emsp;<span class='function'>mainLoop</span><span>();</span><br>" +
+    "&emsp;&emsp;<span class='function'>deinit</span><span>();</span><br>" +
+    "&emsp;&emsp;<span class=\"keyword\">return </span><span class=\"number\">0</span><span>;</span><br>" +
+    "<span>}</span>",
 
     "<p class='comment'>// main.cs</p>" +
-    "<span class='comment'>// Eventually, there will be something here</span><br>" +
-    "<span class='comment'>// JoshEngine does not have C# support yet</span><br>" +
-    "<span class='comment'>// But it will be added in the future (hopefully)</span><br>" +
-    "<span class='comment'>// For now, you can use C++, or port it to C# yourself</span><br>" +
-    "<span class='comment'>// Here is some lorem ipsum for you:</span><br>" +
-    "<span class='comment'>// Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span><br>" +
-    "<span class='comment'>// Donec quis aliquam eros. Mauris vitae fermentum nisl.</span><br>" +
-    "<span class='comment'>// Maecenas posuere velit sed augue pellentesque, id blandit mi euismod.</span><br>" +
-    "<span class='comment'>// Suspendisse accumsan nunc in arcu eleifend accumsan nec a dui.</span><br>" +
-    "<span class='comment'>// Integer condimentum urna posuere leo ultricies pulvinar.</span><br>" +
-    "<span class='comment'>// In consequat egestas interdum.</span><br>" +
-    "<span class='comment'>// Curabitur gravida maximus.</span><br><br>",
+    "<span class='keyword'>using </span><span class='namespace'>System</span><span>;</span><br><br>" +
+    "<span class='keyword'>class </span><span class='type'>Program</span><span> {</span><br><br>" +
+    "&emsp;&emsp;<span class='keyword'>public static </span><span class='type'>void</span><span class='function'> Main</span>" +
+    "<span>(</span><span class='type'>string</span><span>[]</span><span class='var'> args</span><span>) {</span><br><br>" +
+    "&emsp;&emsp;&emsp;&emsp;<span class='type'>Console</span><span>.</span><span class='func-props'>WriteLine</span><span>(</span><span class='string'>\"Hello World!\"</span><span>);</span><br><br>" +
+    "&emsp;&emsp;<span>}</span><br><br>" +
+    "<span>}</span>",
 
     "<p class='comment'># main.py</p>" +
-    "<span class='comment'># Eventually, there will be something here</span><br>" +
-    "<span class='comment'># JoshEngine does not have C# support yet</span><br>" +
-    "<span class='comment'># But it will be added in the future (hopefully)</span><br>" +
-    "<span class='comment'># For now, you can use C++, or port it to C# yourself</span><br>" +
-    "<span class='comment'># Here is some lorem ipsum for you:</span><br>" +
-    "<span class='comment'># Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span><br>" +
-    "<span class='comment'># Donec quis aliquam eros. Mauris vitae fermentum nisl.</span><br>" +
-    "<span class='comment'># Maecenas posuere velit sed augue pellentesque, id blandit mi euismod.</span><br>" +
-    "<span class='comment'># Suspendisse accumsan nunc in arcu eleifend accumsan nec a dui.</span><br>" +
-    "<span class='comment'># Integer condimentum urna posuere leo ultricies pulvinar.</span><br>" +
-    "<span class='comment'># In consequat egestas interdum.</span><br>" +
-    "<span class='comment'># Curabitur gravida maximus.</span><br><br>",
+    "<span class='keyword'>import </span><span class='var'>sys</span><br><br>" +
+    "<span class='keyword'>class</span><span class='type'> Main</span><span>:</span><br><br>" +
+    "&emsp;&emsp;<span class='keyword'>def </span><span class='function'>main</span><span>" +
+    "(</span><span class='var'>self</span><span>, </span><span class='var'>args</span>" +
+    "<span>: </span><span class='type'>list</span><span>[</span><span class='type'>str</span><span>]</span><span>):</span><br><br>" +
+    "&emsp;&emsp;&emsp;&emsp;<span class='function'>print</span><span>(</span><span class='string'>\"Hello world!\"</span><span>)</span><br><br><br>" +
+    "<span class='var'>argv</span><span> = </span><span class='var'>sys</span><span>.</span><span class='property'>argv</span><br><br>" +
+    "<span class='var'>argv</span><span>.</span><span class='func-props'>pop</span><span>(</span><span class='number'>0</span><span>)</span><br><br>" +
+    "<span class='function'>Main</span><span>().</span><span class='func-props'>main</span><span>(</span><span class='var'>argv</span><span>);</span>"
 ]
 
 function langClick(id) {
